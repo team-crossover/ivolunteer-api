270e8722db186f765da59ed926aa19a8aa3be26d
NelsonWilliam <nelsonwilliam@live.com>
2019-06-23 22:26:27 -0300

    Adição dos campos de imagens

--- src/main/java/com/crossover/ivolunteer/presentation/controller/everyone/ImagensController.java
+++ src/main/java/com/crossover/ivolunteer/presentation/controller/everyone/ImagensController.java
@@ -3,6 +3,7 @@ package com.crossover.ivolunteer.presentation.controller.everyone;
 import com.crossover.ivolunteer.business.entity.Imagem;
 import com.crossover.ivolunteer.business.service.ImagemService;
 import com.crossover.ivolunteer.presentation.constants.ApiPaths;
+import com.crossover.ivolunteer.presentation.dto.ImagemDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +18,11 @@ public class ImagensController {
     private ImagemService imagemService;
 
     @GetMapping(ApiPaths.V1.IMAGENS_PREFIX + "/{id}")
-    private Imagem get(@PathVariable("id") long id) {
+    private ImagemDto get(@PathVariable("id") long id) {
         Imagem img = imagemService.findById(id);
         if (img == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Imagem not found");
-        return null;
+        return new ImagemDto(img);
     }
 
 }
