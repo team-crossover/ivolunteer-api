1a60576f17974202dea11f57a878b6cfe4fe72fc
João Pedro Vieira <jp-a.v@hotmail.com>
2019-07-04 14:20:18 -0300

    Acréscimo de dados iniciais

--- src/main/java/com/crossover/ivolunteer/data/config/InitialDataLoader.java
+++ src/main/java/com/crossover/ivolunteer/data/config/InitialDataLoader.java
@@ -13,6 +13,7 @@ import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.EventListener;
 
 /**
  * Adds initial data to the database when the system is initialized.
@@ -90,17 +91,17 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
         ong1.setAreas(Arrays.asList("Animais", "Meio ambiente"));
         ong1.setDataFundacao(LocalDate.of(2005, 8, 6));
         ong1.setEmail("contato@clubedogato.com");
-        ong1.setUrlFacebook("www.facebook.com/clubedogato");
-        ong1.setUrlWebsite("www.clubedogato.com");
+        ong1.setUrlFacebook("fb.com/clubedogato");
+        ong1.setUrlWebsite("clubedogato.com");
         ong1.setTelefone("(62) 3030-3030");
         Evento evento1 = new Evento();
         evento1.setNome("Castração de gatos");
-        evento1.setDescricao("Remoção de testículos gratuita, apenas para gatos e gatas");
+        evento1.setDescricao("Esterilização gratuita de felinos.");
         evento1.setAreas(Arrays.asList("Animais"));
         evento1.setDataRealizacao(LocalDateTime.of(2019, 1, 5, 18, 30));
         Evento evento2 = new Evento();
         evento2.setNome("Adoção de gatos");
-        evento2.setDescricao("Feira de adoção de gatos resgatados da rua, todos vacinados e castrados");
+        evento2.setDescricao("Feira de adoção de gatos resgatados da rua, todos vacinados e castrados.");
         evento2.setAreas(Arrays.asList("Animais"));
         evento2.setDataRealizacao(LocalDateTime.of(2019, 6, 12, 22, 30));
         saveInitialOng("ong", endereco1, ong1, evento1, evento2);
@@ -119,15 +120,49 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
         ong2.setAreas(Arrays.asList("Cultura e arte", "Educação"));
         ong2.setDataFundacao(LocalDate.of(2011, 1, 2));
         ong2.setEmail("amigos@damusica.com");
-        ong2.setUrlFacebook("www.facebook.com/amigosdamusica");
-        ong2.setUrlWebsite("www.amigosdamusica.com");
+        ong2.setUrlFacebook("fb.com/amigosdamusica");
+        ong2.setUrlWebsite("amigosdamusica.com");
         ong2.setTelefone("(62) 12345-1234");
         Evento evento3 = new Evento();
-        evento3.setNome("Roda de samba");
-        evento3.setDescricao("Vai ter rodas e vai ter sambas");
+        evento3.setNome("Introdução à leitura de partitura");
+        evento3.setDescricao("Vamos ensinar métrica e o tempo das notas, não perca!");
         evento3.setAreas(Arrays.asList("Cultura e arte"));
         evento3.setDataRealizacao(LocalDateTime.of(2019, 10, 5, 18, 30));
-        saveInitialOng("ong2", endereco2, ong2, evento3);
+        Evento evento4 = new Evento();
+        evento4.setNome("O que é apropriação cultural?");
+        evento4.setDescricao("Palestra - Mitos e verdades sobre apropriação cultural.");
+        evento4.setAreas(Arrays.asList("Cultura e arte"));
+        evento4.setDataRealizacao(LocalDateTime.of(2019, 12, 12, 14, 30));
+        saveInitialOng("ong2", endereco2, ong2, evento3, evento4);
+
+        // ONG 3
+        Endereco endereco3 = new Endereco();
+        endereco3.setUf("GO");
+        endereco3.setCidade("Goiânia");
+        endereco3.setCep("74305000");
+        endereco3.setBairro("Setor Chyevené");
+        endereco3.setComplemento1("Avenida das Laranjeiras");
+        Ong ong3 = new Ong();
+        ong3.setNome("Paz Laranja");
+        ong3.setDescricao("Militando contra o desmatamento e emissão de poluentes.");
+        ong3.setAreas(Arrays.asList("Meio Ambiente", "Saúde"));
+        ong3.setDataFundacao(LocalDate.of(1997, 4, 4));
+        ong3.setEmail("laranjinha@pazl.com");
+        ong3.setUrlFacebook("fb.com/pazlaranja");
+        ong3.setUrlWebsite("pazlaranja.com");
+        ong3.setTelefone("(62) 12345-6651");
+        Evento evento5 = new Evento();
+        evento5.setNome("Por que usar protetor solar?");
+        evento5.setDescricao("Palestra sobre os efeitos da luz solar na pele");
+        evento5.setAreas(Arrays.asList("Saúde"));
+        evento5.setDataRealizacao(LocalDateTime.of(2019, 12, 12, 14, 30));
+        Evento evento6 = new Evento();
+        evento6.setNome("Distribuição do alerta laranja");
+        evento6.setDescricao("Palestra sobre o desmatamento do cerrado.");
+        evento6.setAreas(Arrays.asList("Meio Ambiente"));
+        evento6.setDataRealizacao(LocalDateTime.of(2019, 7, 7, 13, 50));
+        saveInitialOng("ong3", endereco3, ong3, evento5, evento6);
+
     }
 
     private void saveInitialOng(String username, Endereco endereco, Ong ong, Evento... eventos) {
